d2b4f2d514661a46a7c8d2710dce44d20b3e224b
Annika Harmsen <annikat.harmsen1000@gmail.com>
2025-08-25 14:51:08 +0200

    updated cors policy
    allowed methods post -> post, options
    allowed origins -> https only -> https and http request domains and www aliases

--- config/cors.php
+++ config/cors.php
@@ -3,11 +3,13 @@
 return [
     'paths' => ['api', 'api/*'],
 
-    'allowed_methods' => ['POST'],
+    'allowed_methods' => ['POST', 'OPTIONS'],
 
     'allowed_origins' => [
-        'https://' . env('REQUEST_DOMAIN'),     // Production frontend
-        'https://www.' . env('REQUEST_DOMAIN'), // WWW alias
+        'https://' . env('REQUEST_DOMAIN'),     // Production frontend (https)
+        'https://www.' . env('REQUEST_DOMAIN'), // WWW alias (https)
+        'http://' . env('REQUEST_DOMAIN'),     // Production frontend (http)
+        'http://www.' . env('REQUEST_DOMAIN'), // WWW alias (http)
     ],
 
     'allowed_headers' => ['*'],
